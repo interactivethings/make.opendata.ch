5c3de53925d6f7e1d2ea939e3c4453ce5a83af61
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2011-09-09 14:24:32 +0200

    Added: Javascript video loading

--- assets/app/javascripts/base.js
+++ assets/app/javascripts/base.js
@@ -9,5 +9,27 @@
     }, {
       offset: 15
     });
+    
+    /* LOAD VIDEO
+    /////////////////////////////////////////////////////////////////*/
+    /*$('div.video img').click(function() {
+        getVideo($(this).attr("id"));
+    });*/
+    
   });
 }).call(this);
+
+function getVideo(videoId) {
+    $.ajax({
+        type: "GET",
+        url: "../scripts/video.php",
+        data: "id=" + videoId,
+        success: function(code) {
+            showVideo(videoId, code);
+        }
+    });
+}
+
+function showVideo(videoId, code) {
+    $('#' + videoId).parent('div.video').html(code);
+}
