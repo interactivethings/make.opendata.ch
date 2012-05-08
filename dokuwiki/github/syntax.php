62d08ce8c6b44b344ee829974bcb58e648cd3b16
Oleg Lavrovsky <oleg@utou.ch>
2012-05-08 10:54:26 +0200

    Initial commit of GitHub widget

---
+++ dokuwiki/github/syntax.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * GitHub plugin
+ *
+ * Syntax: <github repo-link> - will be replaced with a GitHub widget
+ * e.g. <github loleg/make.opendata.ch>
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     seism <se1sm@yahoo.ca>
+ */
+ 
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+ 
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_github extends DokuWiki_Syntax_Plugin {
+ 
+   /**
+    * Get an associative array with plugin info.
+    *
+    * <p>
+    * The returned array holds the following fields:
+    * <dl>
+    * <dt>author</dt><dd>Author of the plugin</dd>
+    * <dt>email</dt><dd>Email address to contact the author</dd>
+    * <dt>date</dt><dd>Last modified date of the plugin in
+    * <tt>YYYY-MM-DD</tt> format</dd>
+    * <dt>name</dt><dd>Name of the plugin</dd>
+    * <dt>desc</dt><dd>Short description of the plugin (Text only)</dd>
+    * <dt>url</dt><dd>Website with more information on the plugin
+    * (eg. syntax description)</dd>
+    * </dl>
+    * @param none
+    * @return Array Information about this plugin class.
+    * @public
+    * @static
+    */
+    function getInfo(){
+        return array(
+            'author' => 'seism',
+            'email'  => 'se1sm@yahoo.ca',
+            'date'   => '2011-5-1',
+            'name'   => 'Embed GitHub Plugin',
+            'desc'   => 'Show a GitHUb widget in your wiki',
+            'url'    => 'http://www.dokuwiki.org/wiki:plugins',
+        );
+    }
+ 
+   /**
+    * Get the type of syntax this plugin defines.
+    *
+    * @param none
+    * @return String <tt>'substition'</tt> (i.e. 'substitution').
+    * @public
+    * @static
+    */
+    function getType(){
+        return 'substition';
+    }
+ 
+    /**
+     * What kind of syntax do we allow (optional)
+     */
+//    function getAllowedTypes() {
+//        return array();
+//    }
+ 
+   /**
+    * Define how this plugin is handled regarding paragraphs.
+    *
+    * <p>
+    * This method is important for correct XHTML nesting. It returns
+    * one of the following values:
+    * </p>
+    * <dl>
+    * <dt>normal</dt><dd>The plugin can be used inside paragraphs.</dd>
+    * <dt>block</dt><dd>Open paragraphs need to be closed before
+    * plugin output.</dd>
+    * <dt>stack</dt><dd>Special case: Plugin wraps other paragraphs.</dd>
+    * </dl>
+    * @param none
+    * @return String <tt>'block'</tt>.
+    * @public
+    * @static
+    */
+    function getPType(){
+        return 'block';
+    }
+ 
+   /**
+    * Where to sort in?
+    *
+    * @param none
+    * @return Integer <tt>6</tt>.
+    * @public
+    * @static
+    */
+    function getSort(){
+        return 999;
+    }
+ 
+ 
+   /**
+    * Connect lookup pattern to lexer.
+    *
+    * @param $aMode String The desired rendermode.
+    * @return none
+    * @public
+    * @see render()
+    */
+    function connectTo($mode) {
+      $this->Lexer->addEntryPattern('<github.*?>)',$mode,'plugin_github'); 
+    }
+ 
+    function handle($match, $state, $pos, &$handler){
+        $match = substr($match,10,-1);
+        return array(strtolower($match));
+    }
+ 
+   /**
+    * Handle the actual output creation.
+    *
+    * <p>
+    * The method checks for the given <tt>$aFormat</tt> and returns
+    * <tt>FALSE</tt> when a format isn't supported. <tt>$aRenderer</tt>
+    * contains a reference to the renderer object which is currently
+    * handling the rendering. The contents of <tt>$aData</tt> is the
+    * return value of the <tt>handle()</tt> method.
+    * </p>
+    * @param $aFormat String The output format to generate.
+    * @param $aRenderer Object A reference to the renderer object.
+    * @param $aData Array The data created by the <tt>handle()</tt>
+    * method.
+    * @return Boolean <tt>TRUE</tt> if rendered successfully, or
+    * <tt>FALSE</tt> otherwise.
+    * @public
+    * @see handle()
+    */
+    function render($mode, &$renderer, $data) {
+        $renderer->doc .= '' . $data;
+        return true;
+    }
+}
+ 
+//Setup VIM: ex: et ts=4 enc=utf-8 :
+?>
